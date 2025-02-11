039501c97d74a40d35abb79a6bb7e8a36eddef56
Danny Barake <d.barake@xsarus.nl>
2025-02-11 20:24:31 +0100

    small stuff

--- preload.js
+++ preload.js
@@ -8,3 +8,31 @@ window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('serialport-version').innerText = require('serialport/package').version
 
 })
+
+// Add some browser functions
+const {remote} = require('electron');
+
+document.getElementById('close').addEventListener('click', closeWindow);
+document.getElementById('minimize').addEventListener('click', minimizeWindow);
+document.getElementById('maximize').addEventListener('click', maximizeWindow);
+
+/*
+  getFocusedWindow() may return null.
+  If a user uses global keyboard shortcut to trigger 
+  minimizeWindow, there may not be any window that 
+  is focused right now
+*/ 
+const getWindow = () => remote.BrowserWindow.getFocusedWindow();
+
+function closeWindow () {
+    getWindow().close();
+}
+
+function minimizeWindow () {  
+    getWindow().minimize();
+}
+
+function maximizeWindow () {
+    const window = getWindow();
+    window.isMaximized() ? window.unmaximize() : window.maximize();
+}
